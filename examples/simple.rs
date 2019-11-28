390dba14b36a964c5ded4310d81570bc3f8bbeca
Hideo Hattori <hattori.hideo@panair.jp>
2019-11-29 00:49:31 +0900

    strict

--- examples/simple.rs
+++ examples/simple.rs
@@ -2,5 +2,5 @@ fn main() {
     let s = "Hello world";  /* .unwrap() */
     let _ = s.find("wo").unwrap();
     // let _ = s.find("wo").unwrap();
-    let ignore = "s.unwrap();";
+    let _ignore = "s.unwrap();";
 }
