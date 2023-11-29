dbfbe45908136baae957fa8d2383b0720ce695e5
Hideo Hattori <hhatto.jp@gmail.com>
2023-11-29 12:06:52 +0900

    fmt

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let s = "Hello world";  /* .unwrap() */
+    let s = "Hello world"; /* .unwrap() */
     let _ = s.find("wo").unwrap();
     // let _ = s.find("wo").unwrap();
     let _ignore = "s.unwrap();";
